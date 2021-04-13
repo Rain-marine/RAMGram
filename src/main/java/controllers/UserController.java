969f1f1b568ed0b65961776b5697f01f8785992f
Rain <bahri.zahra79@yahoo.com>
2021-04-14 00:41:09 +0430

    13th

--- src/main/java/controllers/UserController.java
+++ src/main/java/controllers/UserController.java
@@ -5,4 +5,8 @@ import models.User;
 public class UserController {
     User user;
     long userId;
+
+    public void makeNewUser(String fullName, String username, String password, String passwordRe, String email, String phoneNumber, String bio, String birthDate) {
+        user = new User(fullName,username,password,passwordRe,email,phoneNumber,bio,birthDate);
+    }
 }
