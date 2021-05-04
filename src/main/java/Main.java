459ecc90b916d4877b4f6e207e23255182a9a2e5
Rain <bahri.zahra79@yahoo.com>
2021-05-04 23:46:29 +0430

    added javaFX

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,28 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 import views.MenuManager;
-
 import java.util.Scanner;
 
-public class Main {
 
-    public static void main(String[] args)  {
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
         MenuManager manager = new MenuManager(new Scanner(System.in));
         manager.run();
+//        launch(args);
     }
 }
