fd7004a28d008de7eb74917d893e360a10c8e4e4
Rain <bahri.zahra79@yahoo.com>
2021-05-06 03:29:10 +0430

    notif bugs

--- src/main/java/views/Message/ChatMenu.java
+++ src/main/java/views/Message/ChatMenu.java
@@ -30,6 +30,7 @@ public class ChatMenu extends Menu {
     @Override
     public void run() {
         chatController.seeChat(chat);
+        //todo : lazy exception, create getChatById in chat repo
         List<Message> messages = chat.getMessages().stream().sorted(Comparator.comparing(Message::getDate)).collect(Collectors.toList());
         System.out.println(frontUser.getUsername() + "\t" + frontUser.getLastSeen()); // fix last seen
         showMessages(messages);
